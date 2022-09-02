466086d9dabd683645379964f5018de4205fd44a
Matvei <matvesch@protonmail.com>
2022-09-02 12:07:02 +0300

    header is ready

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
+// images & styles
+import './Registration.css';
 import logo from '../../images/logo.png';
+
+// components
 
 const Registration = () => {
     return (
         <div id={'registration'}>
             <header>
                 <img src={logo} alt="logo"/>
-                <h1>פרטי לקוח {'>'} אפשרות משלוח {'>'} אמצעי תשלום</h1>
+                <h1> <span>פרטי לקוח</span> {'>'} אפשרות משלוח {'>'} אמצעי תשלום</h1>
             </header>
         </div>
     );
