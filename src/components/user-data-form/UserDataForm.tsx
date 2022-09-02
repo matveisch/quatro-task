040af9ee9cdf8bfba34a8607f4188e16cf7176a3
Matvei <matvesch@protonmail.com>
2022-09-02 20:21:20 +0300

    validation is done

--- src/components/user-data-form/UserDataForm.tsx
+++ src/components/user-data-form/UserDataForm.tsx
@@ -23,6 +23,11 @@ const SignupSchema = Yup.object().shape({
     email: Yup.string()
         .email('Invalid email')
         .required('Required'),
+    password: Yup.string()
+        .required('Required')
+        .min(8, 'Must contain at least 8 chars'),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Passwords must match'),
     firstName: Yup.string()
         .min(2, 'Too Short!')
         .max(50, 'Too Long!')
@@ -31,12 +36,19 @@ const SignupSchema = Yup.object().shape({
         .min(2, 'Too Short!')
         .max(50, 'Too Long!')
         .required('Required'),
-    password: Yup.string()
-        .required('Required')
-        .min(8, 'Password is too short - should be 8 chars minimum')
-        .matches(/[a-zA-Z]/, 'Password can only contain Latin letters'),
-    confirmPassword: Yup.string()
-        .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    address: Yup.string()
+        .required('Required'),
+    houseNumber: Yup.string()
+        .required('Required'),
+    city: Yup.string()
+        .required('Required'),
+    apartmentNumber: Yup.string()
+        .required('Required'),
+    intercomCode: Yup.string()
+        .required('Required'),
+    phone: Yup.string()
+        .matches(/^[0][5][0|2|3|4|5|9]{1}[-]{0,1}[0-9]{7}$/, 'Must match format 05[x][-][0-9]{7}')
+        .required('Required'),
 });
 
 const UserDataForm = () => {
@@ -77,20 +89,23 @@ const UserDataForm = () => {
                             <div className="input">
                                 <label htmlFor="email"><span>*</span>מייל</label>
                                 <Field id="email" name="email" type="email"/>
+                                {errors.email && touched.email ? (
+                                    <div className="errors">{errors.email}</div>
+                                ) : null}
                             </div>
                             <div className="grid">
                                 <div className="input">
                                     <label htmlFor="password"><span>*</span>בחר סיסמה</label>
                                     <Field id="password" name="password" placeholder="*******" />
                                     {errors.password && touched.password ? (
-                                        <div>{errors.password}</div>
+                                        <div className="errors">{errors.password}</div>
                                     ) : null}
                                 </div>
                                 <div className="input">
                                     <label htmlFor="confirmPassword"><span>*</span>אימות סיסמה</label>
                                     <Field id="confirmPassword" name="confirmPassword" placeholder="*******" />
                                     {errors.confirmPassword && touched.confirmPassword ? (
-                                        <div>{errors.confirmPassword}</div>
+                                        <div className="errors">{errors.confirmPassword}</div>
                                     ) : null}
                                 </div>
                             </div>
@@ -106,41 +121,59 @@ const UserDataForm = () => {
                                     <label htmlFor="firstName"><span>*</span>שם פרטי</label>
                                     <Field id="firstName" name="firstName" />
                                     {errors.firstName && touched.firstName ? (
-                                        <div>{errors.firstName}</div>
+                                        <div className="errors">{errors.firstName}</div>
                                     ) : null}
                                 </div>
                                 <div className="input">
                                     <label htmlFor="secondName"><span>*</span>שם משפחה</label>
                                     <Field id="secondName" name="secondName" />
                                     {errors.secondName && touched.secondName ? (
-                                        <div>{errors.secondName}</div>
+                                        <div className="errors">{errors.secondName}</div>
                                     ) : null}
                                 </div>
                                 <div className="input">
                                     <label htmlFor="address"><span>*</span>כתובת- רחוב, מס׳ בית, עיר</label>
                                     <Field id="address" name="address" placeholder="בזל"/>
+                                    {errors.address && touched.address ? (
+                                        <div className="errors">{errors.address}</div>
+                                    ) : null}
                                 </div>
                                 <div className="input">
                                     <label htmlFor="houseNumber"><span>*</span>מס׳ בית</label>
                                     <Field id="houseNumber" name="houseNumber" placeholder="2"/>
+                                    {errors.houseNumber && touched.houseNumber ? (
+                                        <div className="errors">{errors.houseNumber}</div>
+                                    ) : null}
                                 </div>
                                 <div className="input">
                                     <label htmlFor="city"><span>*</span><span>*</span>עיר</label>
                                     <Field id="city" name="city" placeholder="תל אביב יפו"/>
+                                    {errors.city && touched.city ? (
+                                        <div className="errors">{errors.city}</div>
+                                    ) : null}
                                 </div>
                                 <div className="mini-inputs">
                                     <div className="input mini-input">
                                         <label htmlFor="apartmentNumber"><span>*</span>דירה / כניסה</label>
                                         <Field id="apartmentNumber" name="apartmentNumber" placeholder="1"/>
+                                        {errors.apartmentNumber && touched.apartmentNumber ? (
+                                            <div className="errors">{errors.apartmentNumber}</div>
+                                        ) : null}
                                     </div>
                                     <div className="input mini-input">
                                         <label htmlFor="intercomCode"><span>*</span>קוד לבניין</label>
                                         <Field id="intercomCode" name="intercomCode" placeholder="1234#"/>
+                                        {errors.intercomCode && touched.intercomCode ? (
+                                            <div className="errors">{errors.intercomCode}</div>
+                                        ) : null}
                                     </div>
                                 </div>
                                 <div className="input">
                                     <label htmlFor="phone"><span>*</span><span>*</span>טלפון</label>
                                     <Field id="phone" name="phone" placeholder="050-00223356" type="phone"/>
+                                    {errors.phone && touched.phone ? (
+                                        <div className="errors">{errors.phone}</div>
+                                    ) : null}
                                 </div>
                             </div>
                         </div>
