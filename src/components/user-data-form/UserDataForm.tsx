e6064a274b2360667274391a039cac6408d0129a
Matvei <matvesch@protonmail.com>
2022-09-02 18:06:13 +0300

    restyling with grid

--- src/components/user-data-form/UserDataForm.tsx
+++ src/components/user-data-form/UserDataForm.tsx
@@ -13,11 +13,8 @@ interface Values {
 
 const UserDataForm = () => {
     return (
-        <div id={'user-data-form'}>
-            <h1 id={'login-question'}>האם ברשותך חשבון באתר? התחבר/י לחשבון</h1>
-            <header>
-                <h1>פרטי משתמש</h1>
-            </header>
+        <div id='user-data-form'>
+            <h1 id='login-question'>האם ברשותך חשבון באתר? <span>התחבר/י לחשבון</span></h1>
             <Formik
                 initialValues={{
                     email: '',
@@ -36,12 +33,13 @@ const UserDataForm = () => {
                 }}
             >
                 <Form>
-                    <div id={'user-data'}>
+                    <h1 className='header'>פרטי משתמש</h1>
+                    <div className='data-inputs'>
                         <div className="input">
                             <label htmlFor="email"><span>*</span>מייל</label>
                             <Field id="email" name="email" type="email"/>
                         </div>
-                        <div id="passwords">
+                        <div className="grid">
                             <div className="input">
                                 <label htmlFor="password"><span>*</span>בחר סיסמה</label>
                                 <Field id="password" name="password" placeholder="*******" />
@@ -52,9 +50,45 @@ const UserDataForm = () => {
                             </div>
                         </div>
                     </div>
-
-                    <label htmlFor="subscription">אני מאשר/ת קבלת פרסומים למייל</label>
-                    <Field id="subscription" name="subscription" type="checkbox"/>
+                    <div id="checkbox">
+                        <label htmlFor="subscription">אני מאשר/ת קבלת פרסומים למייל</label>
+                        <Field id="subscription" name="subscription" type="checkbox"/>
+                    </div>
+                    <h1 className="header">כתובת למשלוח</h1>
+                    <div className="data-inputs">
+                        <div className="grid">
+                            <div className="input">
+                                <label htmlFor="first-name"><span>*</span>שם פרטי</label>
+                                <Field id="first-name" name="first-name" />
+                            </div>
+                            <div className="input">
+                                <label htmlFor="second-name"><span>*</span>שם משפחה</label>
+                                <Field id="second-name" name="second-name" />
+                            </div>
+                            <div className="input">
+                                <label htmlFor="address"><span>*</span>כתובת- רחוב, מס׳ בית, עיר</label>
+                                <Field id="address" name="address" placeholder="בזל"/>
+                            </div>
+                            <div className="input">
+                                <label htmlFor="house-number"><span>*</span>מס׳ בית</label>
+                                <Field id="house-number" name="house-number" placeholder="2"/>
+                            </div>
+                            <div className="input">
+                                <label htmlFor="address"><span>*</span>כתובת- רחוב, מס׳ בית, עיר</label>
+                                <Field id="address" name="address" placeholder="בזל"/>
+                            </div>
+                            <div className="mini-inputs">
+                                <div className="input mini-input">
+                                    <label htmlFor="address"><span>*</span>דירה / כניסה</label>
+                                    <Field id="address" name="address" placeholder="בזל"/>
+                                </div>
+                                <div className="input mini-input">
+                                    <label htmlFor="house-number"><span>*</span>קוד לבניין</label>
+                                    <Field id="house-number" name="house-number" placeholder="2"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </Form>
             </Formik>
         </div>
